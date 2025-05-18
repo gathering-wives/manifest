8d421ccff5f80bdbe592ac44bcdc46a99ef5b098
imunproductive <imunproductive@gmail.com>
2025-05-19 02:21:57 +0300

    implement watchdog

---
+++ watchdog/src/main.rs
@@ -0,0 +1,91 @@
+use std::{path::PathBuf, sync::Arc};
+
+use futures::future::join_all;
+use serde::{Deserialize, Serialize};
+use tokio::io::AsyncWriteExt;
+
+#[derive(Deserialize, Serialize)]
+pub struct ManifestVersion {
+    name: String,
+    branch: String,
+    cdn: Vec<String>,
+    game_id: String,
+    unk_id: String,
+    hash: String,
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let content = std::fs::read_to_string("versions.json")?;
+    let manifest = serde_json::from_str::<Vec<ManifestVersion>>(&content)?;
+
+    let client = reqwest::Client::new();
+    let mut tasks = vec![];
+
+    for version in manifest {
+        let version = Arc::new(version);
+
+        let launcher_client = client.clone();
+        let launcher_version = version.clone();
+        tasks.push(tokio::spawn(async move {
+            download_launcher(launcher_client, launcher_version).await
+        }));
+
+        let game_client = client.clone();
+        let game_version = version.clone();
+        tasks.push(tokio::spawn(async move {
+            download_game(game_client, game_version).await
+        }));
+    }
+
+    join_all(tasks).await;
+
+    Ok(())
+}
+
+pub async fn download_launcher(
+    client: reqwest::Client,
+    version: Arc<ManifestVersion>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let url = format!(
+        "https://{}-{}-gamestarter.kurogame.com/launcher/launcher/{}_{}/{}/index.json",
+        version.branch, version.cdn[0], version.unk_id, version.hash, version.game_id
+    );
+    let path = format!(
+        "launcher/{}_{}_{}/index.json",
+        version.game_id, version.unk_id, version.hash
+    );
+
+    download_file(client, &url, &path).await
+}
+
+pub async fn download_game(
+    client: reqwest::Client,
+    version: Arc<ManifestVersion>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let url = format!(
+        "https://{}-{}-gamestarter.kurogame.com/launcher/game/{}/{}_{}/index.json",
+        version.branch, version.cdn[0], version.game_id, version.unk_id, version.hash
+    );
+    let path = format!(
+        "game/{}_{}_{}/index.json",
+        version.game_id, version.unk_id, version.hash
+    );
+
+    download_file(client, &url, &path).await
+}
+
+pub async fn download_file(
+    client: reqwest::Client,
+    url: &String,
+    path: &String,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let path = PathBuf::from(path);
+    tokio::fs::create_dir_all(path.parent().unwrap()).await?;
+
+    let response = client.get(url).send().await?;
+    let mut file = tokio::fs::File::create(path).await?;
+    file.write_all(response.bytes().await?.as_ref()).await?;
+
+    Ok(())
+}
